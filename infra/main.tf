2f81975ac24d62ed14ac60ba4749e6cf25b24862
rafaelgaravello <shokopoka@gmail.com>
2025-05-25 17:48:44 +1000

    changed port for ingress on 3000

--- infra/main.tf
+++ infra/main.tf
@@ -52,8 +52,8 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
